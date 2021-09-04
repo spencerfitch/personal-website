613d4ff78758b7a9f63902c6b9b6b9ac9f91c75a
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-04 14:19:49 -0500

    Added style and className passthrough

--- src/components/card.js
+++ src/components/card.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { card } from '../styles/card.module.css'
 
-const Card = ({ children }) => {
+const Card = ({ children, className, style }) => {
   return (
-    <div className={`${card} text-center rounded shadow`}>
+    <div className={`${card} text-center rounded shadow ${className}`} style={style}>
       {children}
     </div>
   )
