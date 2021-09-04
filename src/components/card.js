53fa9b14d7ecad40892667d1d9bb9d9397a8b8a6
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-03 19:04:39 -0500

    Simplified card styling

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { card, cardContainer } from '../styles/card.module.css'
+import { card } from '../styles/card.module.css'
 
 const Card = ({ children }) => {
   return (
-    <div className={cardContainer}>
-      <div className={`${card} text-center rounded shadow`}>
-        {children}
-      </div>
+    <div className={`${card} text-center rounded shadow`}>
+      {children}
     </div>
   )
 }
