4a1c94f84f879d082849e5a3a47d02763df7780c
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-07 18:18:05 -0500

    Added custom favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,49 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "personal-website",
+    siteUrl: `https://www.yourdomain.tld`,
+    title: `personal-website`,
   },
   plugins: [
-    "gatsby-plugin-sitemap",
+    `gatsby-plugin-sitemap`,
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "src/images/icon.png",
+        name: `Spencer Fitch Portfolio`,
+        short_name: `Spencer Fitch`,
+        start_url: `/`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/favicons/favicon.ico`,
+            sizes: `48x48 32x32 24x24 16x16`,
+            type: `image/x-icon`
+          },
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`
+          },
+          {
+            src: `/favicons/apple-touch-icon.png`,
+            sizes: `180x180`,
+            type: `image/png`
+          }
+
+        ],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "projects",
+        name: `projects`,
         path: `${__dirname}/content/projects`
       }
     },
-    "gatsby-plugin-mdx",
+    `gatsby-plugin-mdx`,
   ],
 };
