41468ade4a846853453a0db8f679bf999d3ad2c3
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-08 22:42:06 -0500

    Moved Section styling to CSS module

--- src/components/section.js
+++ src/components/section.js
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import { section, sectionTitle } from '../styles/section.module.css';
 
 const Section = ({ name, title, children}) => {
   return (
-    <div id={`${name}`.toLowerCase()} style={{paddingTop: '4em', minHeight: '100vh'}}>
-      {title && <h1>{name}</h1>}
+    <div id={`${name}`.toLowerCase()} className={section}>
+      {title && (
+        <h1 className={sectionTitle}>
+          {name}
+        </h1>
+      )}
       {children}
     </div>
   )
