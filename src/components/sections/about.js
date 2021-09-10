63af848a4244070a2ae36d28ad45b6caacdf2164
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-10 00:50:39 -0500

    Updated about with text and portrait

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,12 +1,38 @@
 import * as React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { StaticImage } from 'gatsby-plugin-image';
 import Education from '../education';
+import { aboutContainer, aboutText, portrait } from '../../styles/about.module.css';
 
 
 const About = () => {
+  const aboutQuery = graphql`
+    query {
+      mdx(fileAbsolutePath: {regex: "/content/about/about.mdx/"}) {
+        body
+      }
+    }
+  `;
   
   return (
-    <div>
-      <Education />
+    <div className={aboutContainer}>
+      <div className={aboutText}>
+        <StaticQuery
+          query={aboutQuery}
+          render={data => (
+            <MDXRenderer>
+              {data.mdx.body}
+            </MDXRenderer>
+          )}
+        />
+      </div>
+      <StaticImage
+        src="../../images/portrait.jpg" 
+        alt="Portrait of Spencer Fitch" 
+        className={`${portrait}`}
+        placeholder='blurred'
+      />
     </div>
   );
 }
