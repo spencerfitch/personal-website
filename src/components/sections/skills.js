ac898dbfb2d9f62b473051bc300ba2e8ab5b65fc
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-10 20:55:18 -0500

    Stylized Skills section

--- src/components/sections/skills.js
+++ src/components/sections/skills.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
-import { languageContainer, language, experience, libraries } from '../../styles/skills.module.css';
+import Card from '../card';
+import Icon from '../icon';
+import { skillContainer, centerContent, language, languageTitle, experience, tool } from '../../styles/skills.module.css';
+
 
 class Skills extends React.Component {
   render() {
-    const languageSkillQuery = graphql`
+    const skillQuery = graphql`
       query {
         allMdx(
           filter: {fileAbsolutePath: {regex: "/skills/languages/"}}
@@ -13,46 +16,79 @@ class Skills extends React.Component {
           nodes {
             frontmatter {
               language
+              icon
               experience
               libraries
             }
           }
         }
+        mdx(
+          fileAbsolutePath: {regex: "/content/skills/tools.mdx/"}
+        ) {
+          frontmatter {
+            tools
+          }
+        }
       }
     `;
 
-    const experienceLabels = ['None', 'Familiar', 'Intermediate', 'Intermediate', 'Experienced', 'Expert']
+    const experienceLabels = ['None', 'Proficient', 'Intermediate', 'Experienced', 'Advanced', 'Expert'];
+    const toolIconName = (tool) => (tool.replace(/\s+/g, '-').toLowerCase());
 
     return (
-      <div>
-        <StaticQuery
-          query={languageSkillQuery}
-          render={data => (
-            data.allMdx.nodes.map((lang, idx) => (
-              <div key={idx} className={languageContainer}>
-                <h4 className={language}>
-                  {lang.frontmatter.language}
-                </h4>
-                <div className={`progress ${experience}`}>
-                  <div 
-                    className="progress-bar" 
-                    role="progressbar" 
-                    style={{width: `${(lang.frontmatter.experience/5*100).toFixed(0)}%`}} 
-                    aria-valuenow={lang.frontmatter.experience} 
-                    aria-valuemin="0" 
-                    aria-valuemax="5"
-                  >
-                    {experienceLabels[lang.frontmatter.experience]}
+      <StaticQuery
+        query={skillQuery}
+        render={data => (
+          <div className={skillContainer}>
+            <Card className={centerContent}>
+              <h2>Languages</h2>
+
+              <div>
+                {data.allMdx.nodes.map((lang, idx) => (
+                  <div key={idx} className={language}>
+
+                    <h4 className={languageTitle} >
+                      <Icon name={lang.frontmatter.icon} size="1.2em"/>
+                      <span>{lang.frontmatter.language}</span>
+                    </h4>
+
+                    <div className={`progress ${experience}`}>
+                      <div 
+                        className="progress-bar" 
+                        style={{width: `${(lang.frontmatter.experience/5*100).toFixed(0)}%`}} 
+                        aria-valuenow={lang.frontmatter.experience} 
+                        aria-valuemin="0" 
+                        aria-valuemax="5"
+                      >
+                        {experienceLabels[lang.frontmatter.experience]}
+                      </div>
+                    </div>
+
+                    <p>
+                      {lang.frontmatter.libraries && lang.frontmatter.libraries.join(', ')}
+                    </p>
+
                   </div>
-                </div>
-                <p className={libraries}>
-                  {lang.frontmatter.libraries && lang.frontmatter.libraries.join(', ')}
-                </p>
+                ))}  
               </div>
-            ))
-          )}
-        />
-      </div>
+            </Card>
+
+            <Card className={centerContent}>
+              <h2>Tools and Platforms</h2>
+              
+              <div>
+                {data.mdx.frontmatter.tools.map((elem, idx) => (
+                  <h4 className={tool} key={idx}>
+                    <Icon name={toolIconName(elem)} size="1.2em" />
+                    <span>{elem}</span>
+                  </h4>
+                ))}
+              </div>
+            </Card>
+
+          </div>
+        )}
+      />
     )
   }
 }
